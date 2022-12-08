acd55ffdbabbd1a2431c5ac9e134813d21fb8e32
fazaamal <fazasophian123@gmail.com>
2022-12-08 10:31:04 +0800

    sec

--- project.asm
+++ project.asm
@@ -1,19 +1,36 @@
 .data
 welcome: .asciiz "Welcome! This is the FILA parking garage. Please scan your TouchNGo card to enter!"
+cardDetected: .asciiz "Card detected (y/n)? " 
+enterCardBalance: .asciiz "What is your card balance? "
 
 .text
 #t0 = pressure sensor to detect whether car is present or not at the barrier
-#t1 = TnG card reader
+#t1 = RFID card reader
 #t2 = proximity sensor to detect whether car is in the spot or not
 #t3 = LED (red/green)
 
 start:
 la $a0, welcome
-j printString
+jal printString
 
+
+j exit
+
+readInt:
+li $v0, 5
+syscall
+jr $ra
+
+readChar:
+li $v0, 12
+syscall
+jr $ra
 
 printString:
 li $v0, 4
 syscall
-j $ra 
+jr $ra 
 	
+exit:
+li $v0, 10
+syscall
